4ae70c0df0f0039bea4575979041cdc5db7f0155
eoussama <oussama_gtacj@live.fr>
2025-10-01 18:12:39 +0100

    feat: update DATABASES structure and derive DATABASE_IDS from it

--- core/constants/notion.constants.ts
+++ core/constants/notion.constants.ts
@@ -1,3 +1,5 @@
-export const DATABASE_IDS = [
-  "279d999481b3811e8041d1b324f31226",
-] as const;
+export const DATABASES = {
+  CINEMA_TV: "279d999481b3811e8041d1b324f31226",
+} as const;
+
+export const DATABASE_IDS = Object.values(DATABASES);
