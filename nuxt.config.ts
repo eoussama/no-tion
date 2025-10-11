a71b5044a7bb9a03792ad3ba332c8296fd96f506
eoussama <oussama_gtacj@live.fr>
2025-10-11 11:56:17 +0100

    feat: enable TypeScript strict mode and add tsconfig.json for improved type checking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
+  typescript: {
+    typeCheck: true,
+    strict: true,
+  },
+
   runtimeConfig: {
     notionApiKey: "",
     password: "",
