a4d9cf9cedb317e2fec675dff4c6a8d6047b0003
eoussama <oussama_gtacj@live.fr>
2026-02-08 16:31:59 +0100

    feat: added naviguation guard

---
+++ app/middleware/auth.global.ts
@@ -0,0 +1,25 @@
+import { useAuthStore } from "~/stores";
+
+
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const auth = useAuthStore();
+
+  if (!auth.isInitialized) {
+    await auth.check();
+  }
+
+  const isLoginPage = to.path === "/login";
+
+  if (auth.isLoggedIn) {
+    if (isLoginPage) {
+      return navigateTo("/");
+    }
+
+    return;
+  }
+
+  if (!isLoginPage) {
+    return navigateTo("/login");
+  }
+});
