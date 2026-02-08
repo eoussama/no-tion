2530f069fab73c01b5ccd9018987ce3772eb15f9
eoussama <oussama_gtacj@live.fr>
2026-02-08 14:32:42 +0100

    dep: added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,13 @@ import { defineConfig } from "./server";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  
+
   typescript: {
     typeCheck: true,
     strict: true,
   },
-  
+
+  modules: ["@pinia/nuxt"],
   css: ['~~/assets/css/main.css'],
 
   ...defineConfig(),
