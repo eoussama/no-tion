07cce07feae55b34c5591f3622f19cd89d3d87c7
eoussama <oussama_gtacj@live.fr>
2026-02-08 16:54:51 +0100

    chore: linted api routes

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,3 +1,5 @@
+import { env } from "node:process";
+
 import { tryCatch } from "~~/core";
 import { generateToken, isPasswordValid } from "~~/server/utils";
 
@@ -7,15 +9,16 @@ import { generateToken, isPasswordValid } from "~~/server/utils";
  * @description
  * Authentication with signed HTTP cookie using HMAC.
  */
-export default defineEventHandler(async event => {
+export default defineEventHandler(async (event) => {
   const { password } = await readBody(event);
   const cookie = getCookie(event, "session");
-  
+
   if (cookie) {
     throw createError({ status: 403, message: "User already logged-in", statusText: "Forbidden" });
   }
 
   const [passwordErr, isValid] = await tryCatch(async () => isPasswordValid(password));
+
   if (passwordErr) {
     throw createError({ status: 501, message: "Failed to check password", statusText: "Internal Server Error" });
   }
@@ -23,19 +26,20 @@ export default defineEventHandler(async event => {
   if (!isValid) {
     throw createError({ status: 401, message: "Invalid password", statusText: "Unauthorized" });
   }
-  
+
   const [err, token] = await tryCatch(async () => generateToken());
+
   if (err) {
     throw createError({ status: 501, message: "Failed to generate token", statusText: "Internal Server Error" });
   }
-  
+
   setCookie(event, "session", token, {
     path: "/",
     httpOnly: true,
     sameSite: "lax",
     maxAge: 60 * 60 * 2, // 2 hours
-    secure: process.env.NODE_ENV === "production",
+    secure: env.NODE_ENV === "production",
   });
-  
+
   return createResponse(event, true, { message: "Login successful" });
 });
--- server/api/auth/status.get.ts
+++ server/api/auth/status.get.ts
@@ -7,14 +7,15 @@ import { verifyToken } from "~~/server/utils";
  * @description
  * Verify the signed HTTP cookie.
  */
-export default defineEventHandler(async event => {
+export default defineEventHandler(async (event) => {
   const token = getCookie(event, "session");
-  
+
   if (!token) {
     return createResponse(event, false, { message: "Auth Status" });
   }
-  
+
   const [err, isValid] = await tryCatch(async () => verifyToken(token));
+
   if (err) {
     throw createError({ status: 501, message: "Failed to verify token", statusText: "Internal Server Error" });
   }
@@ -22,6 +23,6 @@ export default defineEventHandler(async event => {
   if (!isValid) {
     deleteCookie(event, "session");
   }
-  
+
   return createResponse(event, isValid, { message: "Auth Status" });
 });
