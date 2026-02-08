d0526d1516ee1f69c3fd68c4a74d5868c165ec1d
eoussama <oussama_gtacj@live.fr>
2026-02-08 15:57:54 +0100

    feat: added endpoint for checking auth status

---
+++ server/api/auth/status.get.ts
@@ -0,0 +1,27 @@
+import { tryCatch } from "~~/core";
+import { verifyToken } from "~~/server/utils";
+
+
+
+/**
+ * @description
+ * Verify the signed HTTP cookie.
+ */
+export default defineEventHandler(async event => {
+  const token = getCookie(event, "session");
+  
+  if (!token) {
+    return createResponse(event, false, { message: "Auth Status" });
+  }
+  
+  const [err, isValid] = await tryCatch(async () => verifyToken(token));
+  if (err) {
+    throw createError({ status: 501, message: "Failed to verify token", statusText: "Internal Server Error" });
+  }
+
+  if (!isValid) {
+    deleteCookie(event, "session");
+  }
+  
+  return createResponse(event, isValid, { message: "Auth Status" });
+});
