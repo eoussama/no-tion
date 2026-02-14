172f84c782c56f99cc0f31a11aab96a93cea74bc
eoussama <oussama_gtacj@live.fr>
2026-02-14 17:32:35 +0100

    feat: added lazy api call

--- app/composables/api.composable.ts
+++ app/composables/api.composable.ts
@@ -17,3 +17,18 @@ export function useApi<T, U extends Record<string, unknown> = Record<string, unk
 
   return tryCatch(() => useFetch<TResponse<T>>(endpoint, { body, $fetch: $apiFetch }));
 }
+
+/**
+ * @description
+ * A composable for making API requests without error handling.
+ * It returns the standard `useFetch` response which includes `data`, `error`, and `pending` properties.
+ *
+ * @param endpoint - The API endpoint to call (e.g., "auth/login").
+ * @param body - Optional request body to send with the API call.
+ * @returns The response from `useFetch` which includes `data`, `error`, and `pending`.
+ */
+export function useApiLazy<T, U extends Record<string, unknown> = Record<string, unknown>>(endpoint: string, body?: U) {
+  const { $apiFetch } = useNuxtApp();
+
+  return useFetch<TResponse<T>>(endpoint, { body, $fetch: $apiFetch });
+}
