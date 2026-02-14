cdc2c996244559c8beac9e1511aebbe17216a801
eoussama <oussama_gtacj@live.fr>
2026-02-14 16:54:08 +0100

    chore: linted route util

--- server/utils/route.util.ts
+++ server/utils/route.util.ts
@@ -1,11 +1,11 @@
+import type { EventHandler, EventHandlerRequest, H3Event } from "h3";
 import type { TResponse } from "~~/core";
-import type { EventHandler, EventHandlerRequest } from "h3";
 
 import { tryCatch } from "@eoussama/core";
 
 
 
-async function authGuard(event: any) {
+async function authGuard(event: H3Event) {
   const cookie = getRequestHeader(event, "cookie") ?? "";
   const [err, res] = await tryCatch(async () => $fetch<TResponse<boolean>>("auth/status", { baseURL: "/api", headers: { cookie } }));
 
@@ -14,6 +14,14 @@ async function authGuard(event: any) {
   }
 }
 
-export function definedProtectedRoute<T extends EventHandlerRequest = EventHandlerRequest, D = any>(handler: EventHandler<T, D>): EventHandler<T, D> {
+/**
+ * @description
+ * Defines a protected route that requires authentication.
+ * The `authGuard` function is used to check if the user is authenticated before allowing access to the route handler.
+ *
+ * @param handler The route handler function to be protected.
+ * @returns A new route handler that includes the authentication guard.
+ */
+export function definedProtectedRoute<T extends EventHandlerRequest = EventHandlerRequest, D = unknown>(handler: EventHandler<T, D>): EventHandler<T, D> {
   return defineEventHandler({ onRequest: [authGuard], handler });
 }
