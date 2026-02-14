bfbcc94e7c4f4515daa1a59ff2965e30e81fd0d9
eoussama <oussama_gtacj@live.fr>
2026-02-14 17:22:09 +0100

    fix: proper typing

--- app/composables/api.composable.ts
+++ app/composables/api.composable.ts
@@ -12,7 +12,7 @@ import { tryCatch } from "~~/core";
  * @param body - Optional request body to send with the API call.
  * @returns A tuple of [error, data] where error is any error that occurred and data is the response data.
  */
-export function useApi<T, U extends Record<string, unknown>>(endpoint: string, body?: U) {
+export function useApi<T, U extends Record<string, unknown> = Record<string, unknown>>(endpoint: string, body?: U) {
   const { $apiFetch } = useNuxtApp();
 
   return tryCatch(() => useFetch<TResponse<T>>(endpoint, { body, $fetch: $apiFetch }));
