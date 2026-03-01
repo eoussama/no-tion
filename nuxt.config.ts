405f8364da415b6f8727cc6fcf71d6c469fb5a1b
Oussama Essamadi <oussama.essamadi@gmail.com>
2026-03-01 15:41:08 +0000

    chore: configured nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     strict: true,
   },
 
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@nuxt/ui"],
   css: ["~~/assets/css/main.css"],
 
   ...defineConfig(),
